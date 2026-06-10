9d83877b923c9d3126a3af221ca303c04196fa87
Francesco Gianfelici <francesc.gianfelici@studenti.unicam.it>
2026-06-10 23:45:53 +0200

    Correzione bug dei tasti non visibili sulla schermata di gioco, crato sprite (canvas 40x40)

--- Tabix/src/main/java/it/unicam/cs/mpgc/rpg125956/App.java
+++ Tabix/src/main/java/it/unicam/cs/mpgc/rpg125956/App.java
@@ -15,21 +15,23 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         primaryStage = stage;
-        primaryStage.setTitle("RPG - UniCam 125956");
-        primaryStage.setResizable(false);
+        primaryStage.setTitle("Tabix RPG - UniCam 125956");
+        primaryStage.setResizable(true);
+        primaryStage.setMinWidth(820);
+        primaryStage.setMinHeight(660);
         mostraSchermoCreazione();
         primaryStage.show();
     }
 
     public static void mostraSchermoCreazione() {
         SchermoCreazione schermo = new SchermoCreazione();
-        Scene scena = new Scene(schermo, 400, 320);
+        Scene scena = new Scene(schermo, 420, 340);
         primaryStage.setScene(scena);
     }
 
     public static void mostraSchermoGioco(GestorePartita gestorePartita) {
         SchermoGioco schermo = new SchermoGioco(gestorePartita);
-        Scene scena = new Scene(schermo, 700, 560);
+        Scene scena = new Scene(schermo, 820, 650);
         primaryStage.setScene(scena);
     }
 }
